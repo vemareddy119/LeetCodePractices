4cebaa0ed68069ce01ae6bcd3e95a0089e589399
vemareddy35706 <41448745+vemareddy119@users.noreply.github.com>
2022-07-06 01:01:33 +0530

    119 , 121

---
+++ src/com/leetcode/arrays/easy/BestTimeToBuyAndSellStocks_121.java
@@ -0,0 +1,26 @@
+package src.com.leetcode.arrays.easy;
+
+public class BestTimeToBuyAndSellStocks_121 {
+    public static void main(String[] args) {
+        int[] prices = {2,1,2,1,0,1,2};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int lsf = Integer.MAX_VALUE;
+        int op = 0;
+        int pist = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] < lsf){
+                lsf = prices[i];
+            }
+            pist = prices[i] - lsf;
+            if(op < pist){
+                op = pist;
+            }
+        }
+        return op;
+    }
+    
+}
